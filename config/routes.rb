20e74f3753c83ac0c9e887f1508034100cdcd54e
Brian Kuhar <briankuhar@gmail.com>
2018-03-04 22:57:27 +0000

    Customized portfolio show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolio_items
+  resources :portfolio_items, except: [:show]
+  get 'portfolio_item/:id', to: 'portfolio_items#show', as: 'portfolio_show'
   
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
